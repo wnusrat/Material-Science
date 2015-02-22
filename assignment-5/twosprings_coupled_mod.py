1db6790551c9584698933ac3f935d57f8e7a368e
wnusrat <wnusrat@users.noreply.github.com>
2015-02-22 00:55:09 -0500

    Update and rename assignment-5 to assignment-5/twosprings_coupled_mod.py

---
+++ assignment-5/twosprings_coupled_mod.py
@@ -0,0 +1,119 @@
+
+
+# System parameters
+
+# First the masses
+m1 = 3.0
+m2 = 3.0
+
+# Then the spring constants (both are equal, so use k):
+
+k = 20.0
+
+# Lengths of the springs (assume equal to one)
+
+L1 = 1.0
+L2 = 1.0
+
+# Now the initial conditions. 
+#     x1 and x2 are the initial displacements
+#     v1 and v2 are the initial velocities
+
+
+x1 = 2.0
+x2 = 8.0
+
+v1 = 0.0
+v2 = 0.0
+
+# We'll use a time step of 0.001 s like in question 3
+
+dt = 0.001
+
+# Now define initial conditions for acceleration and time
+
+v = 0.
+a = 0.
+t = 0.
+
+# Use F1, a1, v1, and t for mass 1
+# Use F2, a2, v2, and t for mass 2
+
+for i in range(10000):
+    
+    F1 = -k*x1 + k*(x2-x1)
+    F2 = -k*(x2-x1) + k*(-x2)
+    a1 = F1/m1
+    a2 = F2/m2 
+    
+    v1 = v1 + a1*dt
+    x1 = x1 + v1*dt
+    
+
+    v2 = v2 + a2*dt
+    x2 = x2 + v2*dt
+    
+    t = i*dt
+    
+
+    
+    print t, x1, v1, a1, F1
+    print t, x2, v2, a2, F2
+    # get particle to start at eq'm position and stay at same position
+
+
+# Now for the plot, we need parameters
+
+absolute_err = 1.0e-8
+relative_err = 1.0e-6
+stopping_time = 10.0
+number_points = 250.0
+
+# Lets set up the number of points for the output.
+# A number of 250 points will give a better graph (more points).
+
+t_stop = [stopping_time * float(i) / (number_points - 1) for i in range(number_points)]
+
+# Now we can plot the solution
+
+from numpy import loadtxt
+from pylab import figure, plot, xlabel, grid, hold, legend, title, savefig
+
+from matplotlib.font_manager import FontProperties
+
+t, x1, x2, v1, v2 = loadtxt ('twospring_coupled_mod.dat', unpack = True)
+
+figure (1, figsize = (5, 5))
+
+xlabel ('t')
+grid(True)
+hold(True)
+lw = 1
+
+plot (t, x1, 'r', linewidth=lw)
+plot (t, x2, 'g', linewidth=lw)
+
+title('Displacement of 2 Coupled-Mass Spring Systems(Modified)')
+savefig('Coupled_Springs_Mod.png', dpi = 200)
+
+
+#########Commment############
+
+# As with the previous coupled spring, we first observe that the 
+#   elastic force gives us a total driving force acting on each mass. 
+#   Though they have the same mass, they have different positions, so 
+#   they therefore have 2 accelerations, and thus two different
+#   potential energies and forces.
+
+
+# Now that the springs are further apart, the spring is expanded.
+# We therefore see that the vertical position between two springs become greater. 
+
+# We observe a longer oscillation between the two masses, so the system
+#     takes longer to achieve equilibrium. 
+#     The total energy increases; the potential energy increases, though
+#     the kinetic energy decreases. 
+
+
+
+
