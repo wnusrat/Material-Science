c8a978be678ec405515f5a2d9e5b8ec0504ff89a
wnusrat <wnusrat@users.noreply.github.com>
2015-03-11 14:46:40 -0400

    Update 2dlattice.py

--- assignment7/2dlattice.py
+++ assignment7/2dlattice.py
@@ -1,24 +1,26 @@
 import numpy as np
 
-nx = 10
-ny = 10
+na1 = 10
+na2 = 10
+
+a1 = np.array([1.,0.,0.]) 
+a2 = np.array([0.,1.,0.])
 
 outputFile = open('2Dlattice.xyz', 'w')
 
-
-x1 = np.array([1.,0.,0.]) 
-x2 = np.array([0.,1.,0.])
-
-outputFile.write(str(nx*ny) + ' \n')
-outputFile.write(' \n')
+outputFile.write (str(na1*na2) + '\n') # a string will give the number of atoms
+outputFile.write ('\n')
 
 
-for i in range(nx): #0..10 for nx
-    for j in range(ny): #0 .. for ny
-        R = nx*x1 + ny*x2      # print R
-            outputFile.write ('Y ' + str(R[0]) + ' ' + str(R[1]) + ' ' + str(R[2]) + ' \n')
 
-outputFile.close()
+for n1 in range(na1):  #0, 1, 2 0, 1, 2 for n2
+    for n2 in range(na2): # 0, 1, 2 for n2
+        R = n1*a1 + n2*a2     
+        # print R
+        outputFile.write('X ' + ' ' + str(R[0]) +  ' ' + str(R[1]) + ' ' + str(R[2]) + '\n')
+
+outputFile.close( )
+
 
 #########Commment############
 # This is the same as assignment 1, except the lattice is only modelled for 2 dimensions (x and y),
